0271e2656dfe4933251d3b547a3f63d05f7f0306
Dandya <Aleksandrmen6@gmail.com>
2021-08-20 15:22:43 +0500

    complite very main functions

--- course cpp/lesson 5/BullAndCow/fuctions.h
+++ course cpp/lesson 5/BullAndCow/fuctions.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <random> // для std::random_device и std::mt19937
+
 #ifndef BULL_AND_COW_H
 #define BULL_AND_COW_H
 //*********Work with words************
@@ -22,17 +23,18 @@ const std::string inputWord()
 #define COW 0
 /**
  * searchBullOrCowRU
- * * Function compares char16_t from word1 and returns bull or cow on RU language
+ * * Function compares two bytes from word1 and word2 and returns bull or cow on RU language
+ * ! Russian symbol has size of two bite
  */
-const int searchBullOrCowRU(char16_t symbol,int indexSimbol,std::u16string word2)
+const int searchBullOrCowRU(char byte1, char byte2, int indexSimbol, std::string word2)
 {
-    for(int index = 0; index < word2.length(); index++)
+    for(int index = 0; index < word2.length()-1; index += 2)
     {
-        if(symbol == word2[index] && indexSimbol != index)
+        if(byte1 == word2[index] && byte2 == word2[index + 1] && indexSimbol != index)
         {
             return COW;
         }    
-        if(symbol == word2[index] && indexSimbol == index)
+        if(byte1 == word2[index] && byte2 == word2[index + 1] && indexSimbol == index)
         {
             return BULL;
         }
@@ -42,7 +44,7 @@ const int searchBullOrCowRU(char16_t symbol,int indexSimbol,std::u16string word2
 //----------------------------------------------------------------
 /**
  * searchBullOrCowEN
- * * Function compares char16_t from word1 and returns bull or cow on EN language
+ * * Function compares char from word1 and word2 and returns bull or cow on EN language
  */
 const int searchBullOrCowEN(char symbol,int indexSimbol, std::string word2)
 {
@@ -61,47 +63,44 @@ const int searchBullOrCowEN(char symbol,int indexSimbol, std::string word2)
 } 
 //----------------------------------------------------------------
 /**
- * compareWordsRU
- * * Function compares two words and changes count bulls and cows on RU language
- */
-const void compareWordsRU(std::u16string word1, std::u16string word2,
-        int* countBulls, int* countCows)
-{
-    int result;
-    for(int index = 0; index < word1.length()-1; index++) // not look '\0'
-    {
-        result = searchBullOrCowRU(word1[index],index, word2);
-        if (result == BULL)
-        {
-            (*countBulls)++;
-        }
-        else if (result == COW)
-        {
-            (*countCows)++;
-        }
-    }
-}
-//----------------------------------------------------------------
-/**
- * compareWordsEN
+ * compareWords
  * * Function compares two words and changes count bulls and cows on EN language
  */
-const void compareWordsEN(std::string word1, std::string word2, 
-        int* countBulls, int* countCows)
+const void compareWords(std::string word1, std::string word2, 
+        int* countBulls, int* countCows, int language)
 {
     int result;
-    for(int index = 0; index < word1.length()-1; index++) // not look '\0'
+    if(language == LANGUAGE_EN)
     {
-        result = searchBullOrCowEN(word1[index],index, word2);
-        if (result == BULL)
+        for(int index = 0; index < word1.length()-1; index++) // not look '\0'
         {
-            (*countBulls)++;
-        }
-        else if (result == COW)
-        {
-            (*countCows)++;
+            result = searchBullOrCowEN(word1[index],index, word2);
+            if (result == BULL)
+            {
+                (*countBulls)++;
+            }
+            else if (result == COW)
+            {
+                (*countCows)++;
+            }
         }
     }
+    else // LANGUAGE_RU
+    {
+        for(int index = 0; index < word1.length()-1; index+=2) // not look '\0'
+        {
+            result = searchBullOrCowRU(word1[index],word1[index+1],index,word2);
+            if (result == BULL)
+            {
+                (*countBulls)++;
+            }
+            else if (result == COW)
+            {
+                (*countCows)++;
+            }
+        }
+    }
+    
 }
 //********Work with dictionary*************
 #define EASY 0
@@ -111,28 +110,60 @@ const void compareWordsEN(std::string word1, std::string word2,
  * isTrueDifficultyLevel
  * * Fuction checks word is true for a difficulty level
  */
-bool isTrueDifficultyLevel(int difficultyLevel, int lengthWord)
+bool isTrueDifficultyLevel(int difficultyLevel, int lengthWord, int language)
 {
-    switch(difficultyLevel)
+    if (language == LANGUAGE_EN)
     {
-        case EASY: if(lengthWord <= 4 && lengthWord >= 3) return true; break;
-        case NORMAL: if(lengthWord <= 6 && lengthWord >= 5) return true; break;
-        case HARD: if(lengthWord <= 8 && lengthWord >= 7) return true; break;
+        switch(difficultyLevel)
+        {
+            case EASY: if(lengthWord <= 5 && lengthWord >= 4) return true; break;
+            case NORMAL: if(lengthWord <= 7 && lengthWord >= 6) return true; break;
+            case HARD: if(lengthWord <= 9 && lengthWord >= 8) return true; break;
+        }
+    }
+    else // LANGUAGE_RU ! Russian symbol has size of two bite
+    {
+        switch(difficultyLevel)
+        {
+            case EASY: if(lengthWord <= 10 && lengthWord >= 8) return true; break;
+            case NORMAL: if(lengthWord <= 14 && lengthWord >= 12) return true; break;
+            case HARD: if(lengthWord <= 18 && lengthWord >= 16) return true; break;
+        }
     }
     return false;
 }
 //-------------------------------------------------------------
-std::string randomWordEN(int difficultyLevel)
+#define DICTIONARY_MAX_RU 67774
+#define DICTIONARY_MAX_EN 1775
+std::string randomWord(int difficultyLevel, int language)
 {
-    std::random_device rd; // need for random number 
-    std::mt19937 mersenne(rd()); // initialization algorithm by Mersenne 
-    ifstream dictionary;
-    dictionary.open("./dictionary/engwords.txt");
-    std::string word; 
-    for(int index = mersenne(rd()); index >= 0; index--) // go to random word
+    /*std::random_device rd; // need for random number 
+    std::mt19937 mersenne(rd()); // initialization algorithm by Mersenne */
+    std::ifstream dictionary;
+    int maxWords {0};
+    if(language == LANGUAGE_EN)
     {
-        getline(dictionary, word);
+        dictionary.open("./dictionary/engwords.txt");
+        maxWords = DICTIONARY_MAX_EN;
     }
+    else //LANGUAGE_RU
+    {
+       dictionary.open("./dictionary/ruwords.txt"); 
+       maxWords = DICTIONARY_MAX_RU;
+    }
+    std::string word; 
+    srand(time(0));
+    for(int index = rand()%maxWords; index >= 0; index--) // go to random word
+    {
+        std::getline(dictionary, word);
+    }
+    dictionary.close();
+    if(isTrueDifficultyLevel(difficultyLevel, word.length(), language))
+    {
+        return word;
+    }
+    return randomWord(difficultyLevel,language);
+    
 }
 //*****************************************
 #endif 
--- course cpp/lesson 5/BullAndCow/tests.cpp
+++ course cpp/lesson 5/BullAndCow/tests.cpp
@@ -3,10 +3,12 @@
 
 TEST(Main, SearchBullOrCowRU)
 {
-    std::u16string word = {'П','р','и','в','е','т','\0'};
-    EXPECT_EQ(searchBullOrCowRU('и',2,word), 1);
-    EXPECT_EQ(searchBullOrCowRU('р', 2, word), 0);
-    EXPECT_EQ(searchBullOrCowRU('а', 3, word), -1);
+    std::string word = "Привет";
+    std::string letters = "ират";
+    EXPECT_EQ(searchBullOrCowRU(letters[0], letters[1], 4, word), 1);
+    EXPECT_EQ(searchBullOrCowRU(letters[2], letters[3], 5, word), 0);
+    EXPECT_EQ(searchBullOrCowRU(letters[6], letters[7], 5, word), 0);
+    EXPECT_EQ(searchBullOrCowRU(letters[4], letters[5], 3, word), -1);
 }
 
 TEST(Main, SearchBullOrCowEN)
@@ -19,10 +21,10 @@ TEST(Main, SearchBullOrCowEN)
 
 TEST(Main, CompareWordsRU)
 {
-    std::u16string word1 =  {'П','р','и','в','е','т','\0'};
-    std::u16string word2 =  {'П','и','л','о','т','ы','\0'};
+    std::string word1 = "Привет";
+    std::string word2 =  "Пилоты";
     int countBulls{0}, countCows{0};
-    compareWordsRU(word1, word2, &countBulls, &countCows);
+    compareWords(word1, word2, &countBulls, &countCows, LANGUAGE_RU);
     EXPECT_EQ(countCows, 2);
     EXPECT_EQ(countBulls, 1);
 }
@@ -32,10 +34,41 @@ TEST(Main, CompareWordsEN)
     std::string word1 = "Hello";
     std::string word2 = "Henry";
     int countBulls{0}, countCows{0};
-    compareWordsEN(word1, word2, &countBulls, &countCows);
+    compareWords(word1, word2, &countBulls, &countCows, LANGUAGE_EN);
     EXPECT_EQ(countCows, 0);
     EXPECT_EQ(countBulls, 2);
 }
+
+TEST(Main, RandomWordsEN)
+{
+    std::string word = randomWord(EASY, LANGUAGE_EN);
+    std::cout << word << std::endl;
+    word = randomWord(NORMAL, LANGUAGE_EN);
+    std::cout << word << std::endl;
+    word = randomWord(HARD, LANGUAGE_EN);
+    std::cout << word << std::endl;
+}
+
+TEST(Main, RandomWordsRU)
+{
+    std::string word = randomWord(EASY, LANGUAGE_RU);
+    std::cout << word << std::endl;
+    word = randomWord(NORMAL, LANGUAGE_RU);
+    std::cout << word << std::endl;
+    word = randomWord(HARD, LANGUAGE_RU);
+    std::cout << word << std::endl;
+}
+
+/*TEST(Main, PROSTO)
+{
+    std::u16string word;
+    std::cout << "Input word: ";
+    std::cin >> word;
+    for(int index = 0; index < word.length(); index++)
+    {
+        std::cout << word[index] << std::endl;
+    }
+}*/
 
 int main(int argc, char **argv)
 {
