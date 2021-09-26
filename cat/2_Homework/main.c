9ac3d6d7dd0696bbd784d26349060de33b58612b
Dandya <Aleksandrmen6@gmail.com>
2021-09-26 13:23:19 +0300

    First work on second homework

---
+++ cat/2_Homework/main.c
@@ -0,0 +1,15 @@
+#include "mem.c"
+int main(int argc, char* argv[]) 
+{
+  //mallopt( MALLOC_CHECK_ , 0); 
+  //mtrace();
+  
+
+  void* ptr;
+  for (int i = 0; i<100; i++)
+  {
+    ptr = new(1000);
+  }
+  //free(ptr);
+  //getchar();
+}
---
+++ cat/2_Homework/mem.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <mcheck.h>
+#include <malloc.h>
+
+
+void* new(size_t size)
+{
+    return malloc(size);
+}
+
+static void __attribute__((constructor)) constructor(void)
+{
+    mtrace();
+}
+
+static void __attribute__((destructor)) freeMem(void)
+{
+    FILE* proc = fopen("/proc/self/cmdline", "r");        
+    if(proc == NULL)
+    {
+        printf("proc");
+    }
+    char* cmdline = (char*)malloc(257);
+    fgets(cmdline, 257, proc);
+    close(proc);
+    
+    
+}
+
